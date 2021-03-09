c57443d3935f4d80f9210d264b390fff873b3274
volhashax <volhashax@gmail.com>
2021-03-09 21:12:53 +0300

    feat: all tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+
+if(array === undefined || array.length == 0){
   return 0;
+}
+  return Math.min.apply(Math, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if(array === undefined || array.length == 0){
+    return 0;
+  }
+  return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0){
+    return 0;
+  }  
+  var sum = 0;
+  for(let i = 0; i <array.length; i++){
+    sum += array[i];
+  }
+  return sum/array.length;
 }
